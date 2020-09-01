8ee75e10a1f9cbbfd879e6e2487b1307c2f89579
Alex <alex@nectortele.com>
2020-09-01 02:13:14 -0400

    test secret is working

--- src/fauna.py
+++ src/fauna.py
@@ -17,4 +17,4 @@ def getfam():
     pattern = '\d+'
     result = re.findall(pattern, string)
 
-    return "First Name: " + str([client.query(q.get(q.ref(q.collection('person'), result[0])))['data']][0].get("firstName"))
+    return "First Name: " + str([client.query(q.get(q.ref(q.collection('person'), result[1])))['data']][0].get("firstName"))
